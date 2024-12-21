53a5ae220d4023023ec1ee18ad5382cf895fa478
SolihovAbdulloh <abdullasolikh@mail.com>
2024-12-21 16:45:05 +0500

    Search finished in todo_list

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,19 @@ import React, { useRef, useState } from "react";
 import axios from "axios";
 import { Button, Input, message, notification } from "antd";
 import SecondComponent from "./SecondComponent";
+import Search from "antd/es/input/Search";
 function App() {
-  const Addref = useRef();
+  const Addref = useRef("");
   const queryClient = useQueryClient();
   const [editId, setEditId] = useState(null);
   const [editName, setEditName] = useState("");
-  const [search , setSearch] = useState(null)
+  const [search, setSearch] = useState("");
   const { data, isLoading, isError } = useQuery({
     queryKey: ["TODO"],
     queryFn: () =>
       axios
         .get("https://6750968269dc1669ec1bb204.mockapi.io/comments")
-        .then((data) => data.data),
+        .then((data) =>data.data),
   });
 
   const deleteMutation = useMutation({
@@ -59,26 +60,49 @@ function App() {
     setEditId(null);
   };
 
+  const filtered = data
+    ? data.filter((value) =>
+        value.name.toLowerCase().includes(search.toLowerCase())
+      )
+    : [];
+    console.log(filtered);
+    
+
   return (
     <div className="w-full ">
+      <Search className="flex justify-center mt-5 m-auto items-center w-[30%]"
+        
+        placeholder="Search..."
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <form className="m-auto flex items-end gap-3  w-[30%]" onSubmit={submit}>
-        <Input ref={Addref} className="mt-[30px]  " />
-        <Button htmlType="submit" >Add</Button>
+        <Input placeholder="So'z kiriting..." required ref={Addref} className="mt-[30px]  " />
+        <Button htmlType="submit">Add</Button>
       </form>
       <br />
       {editId && (
-        <form className="w-[30%] flex items-end gap-3 m-auto" onSubmit={submitEdit}>
+        <form
+          className="w-[30%] flex items-end gap-3 m-auto"
+          onSubmit={submitEdit}
+        >
           <Input
             value={editName}
             onChange={(e) => setEditName(e.target.value)}
           />
-          <Button htmlType="submit" onClick={()=>notification.success({message:'Malumot saqlandi'})}>Save</Button>
+          <Button
+            htmlType="submit"
+            onClick={() =>
+              notification.success({ message: "Malumot saqlandi" })
+            }
+          >
+            Save
+          </Button>
         </form>
       )}
       {isLoading ? (
         <p className="text-red-800 text-[43px]">Loading....</p>
       ) : (
-        data?.map((value) => (
+        filtered?.map((value) => (
           <div
             className="w-[30%] mt-3 flex justify-center items-center  gap-[30px] m-auto bg-orange-400 p-4"
             key={value.id}
